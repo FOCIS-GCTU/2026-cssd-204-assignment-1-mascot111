bbd53640c6f58d629a64c187da9815459c364ee9
Lucid <117250815+mascot111@users.noreply.github.com>
2026-05-31 21:06:12 +0000

    Finalized SKA initials

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 2
+#
+# Name: Sylvester Kwabena Ahenkorah
+# STUDENT ID: 2425401090
+# Email: mascotahenkorah192@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Sylvester Kwabena Ahenkorah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Print my initials in stylized block letters.
+    print()
+    print("...SKA")
+    print()
+    print(".....SSSSSSSS..........KKKK....KKKK............AAAA.........")
+    print("....SSSSSSSSSS.........KKKK...KKKK...........AAAAAA.........")
+    print("...SSSS....SSSS........KKKK..KKKK..........AAAA.AAAA........")
+    print("...SSSS................KKKK.KKKK..........AAAA...AAAA.......")
+    print("....SSSSSSSS...........KKKKKKKK...........AAAAAAAAAAAA......")
+    print("......SSSSSSSS.........KKKKKKKK...........AAAAAAAAAAAA......")
+    print("...........SSSS........KKKK.KKKK..........AAAA....AAAA......")
+    print("...SSSS....SSSS........KKKK..KKKK.........AAAA....AAAA......")
+    print("....SSSSSSSSSS....**...KKKK...KKKK....**...AAAA....AAAA...**")
+    print(".....SSSSSSSS.....**...KKKK....KKKK...**...AAAA....AAAA...**")
+    print()
 
 
 main()
